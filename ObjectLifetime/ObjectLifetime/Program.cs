e253fe2613b48f0d712ad577e5741f6dfb3fea94
Brandon <brandoncyu@gmail.com>
2019-08-25 11:30:20 -0700

    static methods

---
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            Car.MyMethod();
+
+            /*
+            myCar.Make = "Oldmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            
+
+            Car myThirdCar = new Car("Hyundai", "Accent", 2013, "Desert Blonde");
+
+            Car myOtherCar = myCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            //this updates myCar also.
+            myOtherCar.Model = "98";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            
+
+            myCar = null;
+            */
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car()
+        {
+            //you can load information from a configuration file, a database, etc.
+            //the "this" keyword is optional.
+            Make = "Nissan";
+
+        }
+
+        //overloaded constructor.
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Call the static MyMethod");
+            Console.WriteLine(Make);
+
+        }
+    }
+}
