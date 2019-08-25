ee19cf93d0cd45527c77e80796b3ffea294d122a
Brandon <brandoncyu@gmail.com>
2019-08-25 12:08:39 -0700

    added references

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyCodeLibrary;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebPage("http://google.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
