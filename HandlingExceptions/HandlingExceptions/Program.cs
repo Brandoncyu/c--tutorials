0206cc9b40e90e85f9d46b79a62b8ce3e30c432c
Brandon <brandoncyu@gmail.com>
2019-08-25 20:14:31 -0700

    added exceptions

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content);
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the directory is named correctly");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There Was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Code to finalize
+                //Setting objects to null
+                //Closing Database connections.
+                Console.WriteLine("Closing application now...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
