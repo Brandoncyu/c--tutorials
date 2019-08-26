f38fd444a6b7e7ddaa6f40c6202e34ae4e655517
Brandon <brandoncyu@gmail.com>
2019-08-25 21:32:53 -0700

    added assemblies and namespaces

---
+++ AssembliesAndNameSpaces/AssembliesAndNameSpaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //full name is System.Console.WriteLine("Hello World!");
+            //looks inside System.
+            Console.WriteLine("Hello World!");
+
+            
+            //WebClient is from System.net
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.miscorsoft.com");
+
+            //File is from System.IO
+            File.WriteAllText(@"C:\Lesson17\WriteText.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
